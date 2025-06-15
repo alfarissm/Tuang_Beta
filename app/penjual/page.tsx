d8ca2abf0a77d1a15c05929ce21ad101c2209919
alfaris <muhamadalfariz2211@gmail.com>
2025-06-16 00:27:20 +0700

    fix ui seller

--- app/penjual/page.tsx
+++ app/penjual/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
+// import Image from "next/image";
 
 // Import components
 import SideNavigation from "./components/SideNavigation";
